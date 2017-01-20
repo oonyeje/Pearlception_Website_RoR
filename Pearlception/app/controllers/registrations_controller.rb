c0564fff147272020b363d5b7dcefc563af49f18
Okechi Onyeje <o.onyeje3321@gmail.com>
2017-01-20 00:51:06 -0500

    Added Dashboard Integration and Authentication
    - Signin and Signup views still not being shown and defaulting to default

--- Pearlception/app/controllers/registrations_controller.rb
+++ Pearlception/app/controllers/registrations_controller.rb
@@ -1,13 +1,23 @@
 class RegistrationsController < Devise::RegistrationsController
   include ApplicationHelper
-  protected
+  #protected
 
   def create
-    super
     binding.pry
+    @user = User.new(sign_up_params)
+    if !@user.admin?
+      #search by company token given by one of pearlception admins to the company
+      #and look up comany_id #
+      @user.company_id = 0
+    else
+      @user.company_id = -1
+    end
+    @user.save
+    sign_in @user
+    redirect_to "/"
   end
 
   def sign_up_params
-    params.require(:user).permit(:company_id, :email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :company_id)
   end
 end
