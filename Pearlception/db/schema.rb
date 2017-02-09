af447f14937fbe43fc6a5b200521d9b44bb44e81
Okechi Onyeje <o.onyeje3321@gmail.com>
2017-02-09 14:09:35 -0500

    schema fix

--- Pearlception/db/schema.rb
+++ Pearlception/db/schema.rb
@@ -22,7 +22,8 @@ ActiveRecord::Schema.define(version: 20170119161737) do
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
-    t.integer  "company_id",             default: -1, null: false
+    t.integer  "company_id",             null: false
+    t.boolean  "admin",                  default: false, null: false 
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
