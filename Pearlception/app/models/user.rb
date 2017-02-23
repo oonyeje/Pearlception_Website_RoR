eab7216058517978134aff3401642eb56b3f7eef
Okechi Onyeje <o.onyeje3321@gmail.com>
2017-02-23 12:18:44 -0500

    Add auto switch to default db for admin user

--- Pearlception/app/models/user.rb
+++ Pearlception/app/models/user.rb
@@ -5,10 +5,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
+
   def after_database_authentication
     if !self.admin
       Apartment::Tenant.switch!(Company.find(self.company_id).company_name.gsub(/'/,'').gsub(/\s/,''))
+    else
+      Apartment::Tenant.switch!
     end
   end
 end
