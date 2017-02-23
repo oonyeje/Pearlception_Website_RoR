4c141c7a2ce488865d6b0e3920a9c666eedbe309
Okechi Onyeje <o.onyeje3321@gmail.com>
2017-02-23 17:09:58 -0500

    Added admin global company lookup for result data
    - currently a bug that is not displaying data

--- Pearlception/app/controllers/dashboard_controller.rb
+++ Pearlception/app/controllers/dashboard_controller.rb
@@ -2,7 +2,22 @@ class DashboardController < ApplicationController
     before_filter :deny_to_visitors
 
     def index
-        @results = Result.page(params[:page]).per(5)
+      #This is a temporary fix until runs and results models are properly discussed with bobby
+        #@results = Result.page(params[:page]).per(5)
+        if current_user.admin?
+          @results = Hash.new
+          names = (Company.pluck :company_name) - ["IVA"]
+          names.map!{|tenant| tenant.gsub(/'/,'').gsub(/\s/,'')}
+          names.each do
+              |name|
+
+              Apartment::Tenant.switch!(name.gsub(/'/,'').gsub(/\s/,''))
+              @results[name.gsub(/'/,'').gsub(/\s/,'')] = Run.page(params[:page]).per(5)
+          end
+          Apartment::Tenant.switch!
+        else
+          @results = Run.page(params[:page]).per(5)
+        end
 
     end
 
