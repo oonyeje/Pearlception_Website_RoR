edbcd9a0e64eace289069611e5f56a5a7281f819
bmv root <bmv@example.com>
2017-02-24 14:56:31 -0500

    merge conflicts

--- Pearlception/app/controllers/dashboard_controller.rb
+++ Pearlception/app/controllers/dashboard_controller.rb
@@ -2,9 +2,6 @@ class DashboardController < ApplicationController
     before_filter :deny_to_visitors
 
     def index
-<<<<<<< HEAD
-        @results = Result.page(params[:page]).per(5)
-=======
       #This is a temporary fix until runs and results models are properly discussed with bobby
         #@results = Result.page(params[:page]).per(5)
         if current_user.admin?
@@ -29,7 +26,6 @@ class DashboardController < ApplicationController
           @results = Run.page(params[:page]).per(5)
         end
 
->>>>>>> 66dfc7d12c7a612332c9b8de8f32df39a0d362ce
     end
 
     def deny_to_visitors
