e4d8fa4223915da415b89111f5e90d576c60a33d
bmv root <bmv@example.com>
2017-04-14 13:22:48 -0400

    change dashboard controller

--- Pearlception/app/controllers/dashboard_controller.rb
+++ Pearlception/app/controllers/dashboard_controller.rb
@@ -8,7 +8,8 @@ class DashboardController < ApplicationController
           puts params
           @results = Hash.new
           names = (Company.pluck :company_name) - ["IVA"]
-          names.map!{|tenant| tenant.gsub(/'/,'').gsub(/\s/,'')}
+          binding.pry
+		  names.map!{|tenant| tenant.gsub(/'/,'').gsub(/\s/,'')}
           names.each do
               |name|
 
